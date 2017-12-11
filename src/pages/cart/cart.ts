2649949c3d0ea0186be5f600473e8224f3c6d666
Andrey Ostanin <kaberc@gmail.com>
2017-12-11 20:55:45 +0400

    Fill Cart page

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from '@angular/core'
+import { IonicPage, NavController, NavParams } from 'ionic-angular'
 
 @IonicPage()
 @Component({
@@ -7,12 +7,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'cart.html',
 })
 export class CartPage {
+  edit: boolean = false
+  totalPrice: number = 52
+  items = [
+    {
+      title: 'Orange Fresh',
+      quantity: 2,
+      price: 12,
+      img: '../assets/imgs/dishes/pizza1.png',
+    },
+    {
+      title: 'Mint Lemonade',
+      quantity: 2,
+      price: 14,
+      img: '../assets/imgs/dishes/pizza1.png',
+    },
+  ]
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CartPage');
+  toggleEdit() {
+    this.edit = !this.edit
   }
 
 }
