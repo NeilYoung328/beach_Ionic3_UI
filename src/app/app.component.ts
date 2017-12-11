5c523a034895ac0fc74384a586c465cac39f1a3e
Andrey Ostanin <kaberc@gmail.com>
2017-12-11 16:46:20 +0400

    Remove obsolete import from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar'
 import { SplashScreen } from '@ionic-native/splash-screen'
 
 import { LoginPage } from '../pages/login/login'
-import { SearchDetailsPage } from '../pages/search-details/search-details'
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = LoginPage
-  // rootPage:any = SearchDetailsPage
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
