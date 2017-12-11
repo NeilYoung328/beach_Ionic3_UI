05841afd66c0c1bd37f1db863969764fa090851d
Andrey Ostanin <kaberc@gmail.com>
2017-12-11 04:06:51 +0400

    Fill Search Results page and add mock data

--- src/pages/search-results/search-results.ts
+++ src/pages/search-results/search-results.ts
@@ -1,25 +1,84 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SearchResultsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-search-results',
   templateUrl: 'search-results.html',
 })
 export class SearchResultsPage {
+  public country: string = 'Romania'
+  public place: string = 'Mamaia'
+  public beaches = [
+    {
+      title: 'Ammos Beach',
+      seats: 410,
+      sunbed: 42,
+      umbrella: 20,
+      baldaquin: 8,
+      reviews: 12,
+      rating: 2,
+      img: '../assets/imgs/beaches/beach1.png',
+      options: ['cash', 'credit', 'food', 'shower', 'wifi'],
+    },
+    {
+      title: 'Crazy Beach',
+      seats: 300,
+      sunbed: 33,
+      umbrella: 11,
+      baldaquin: 1,
+      reviews: 22,
+      rating: 5,
+      img: '../assets/imgs/beaches/beach2.png',
+      options: ['cash', 'tea', 'food', 'shower', 'wifi', 'computer'],
+    },
+    {
+      title: 'Ammos Beach',
+      seats: 410,
+      sunbed: 42,
+      umbrella: 20,
+      baldaquin: 8,
+      reviews: 12,
+      rating: 3,
+      img: '../assets/imgs/beaches/beach1.png',
+      options: ['cash', 'credit', 'tea', 'food', 'shower', 'wifi', 'computer'],
+    },
+    {
+      title: 'Crazy Beach',
+      seats: 300,
+      sunbed: 33,
+      umbrella: 11,
+      baldaquin: 1,
+      reviews: 22,
+      rating: 1,
+      img: '../assets/imgs/beaches/beach2.png',
+      options: ['cash', 'credit', 'tea', 'wifi', 'computer'],
+    },
+    {
+      title: 'Ammos Beach',
+      seats: 410,
+      sunbed: 42,
+      umbrella: 20,
+      baldaquin: 8,
+      reviews: 12,
+      rating: 5,
+      img: '../assets/imgs/beaches/beach1.png',
+      options: ['cash', 'credit', 'tea', 'food'],
+    },
+    {
+      title: 'Crazy Beach',
+      seats: 300,
+      sunbed: 33,
+      umbrella: 11,
+      baldaquin: 1,
+      reviews: 22,
+      rating: 2,
+      img: '../assets/imgs/beaches/beach2.png',
+      options: ['cash', 'shower', 'wifi', 'computer'],
+    },
+  ]
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchResultsPage');
   }
 
 }
