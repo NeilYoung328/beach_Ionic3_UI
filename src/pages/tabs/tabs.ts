30d707ccddaac0b39545b8e6c553f98f81f30e46
Andrey Ostanin <kaberc@gmail.com>
2017-12-11 16:13:04 +0400

    Add slide menu

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
-import { SearchPage } from '../search/search';
-import { CartPage } from '../cart/cart';
+import { SearchPage } from '../search/search'
+import { CartPage } from '../cart/cart'
 
 @Component({
+  selector: 'page-tabs',
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
+  isMenuShown: boolean = false
+
   tab1Root = SearchPage;
   tab2Root = CartPage;
-  // tab3Root = ContactPage;
-  // tab4Root =
-  // tab5Root =
 
   constructor() {
 
+  }
+
+  toggleMenu() {
+    this.isMenuShown = !this.isMenuShown
   }
 }
