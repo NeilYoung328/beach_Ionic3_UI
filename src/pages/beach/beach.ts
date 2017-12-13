01079ea91f2360ec53b20e2e793efc8af42a50d6
Andrey Ostanin <kaberc@gmail.com>
2017-12-13 10:16:52 +0400

    Add initial layout on Beach page

--- src/pages/beach/beach.ts
+++ src/pages/beach/beach.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from '@angular/core'
+import { IonicPage, NavController, NavParams } from 'ionic-angular'
 
 @IonicPage()
 @Component({
@@ -8,6 +8,71 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BeachPage {
   beachSection: string = 'overview'
+  menu = [
+    {
+      category: 'sandwiches',
+      itemsShown: false,
+      items: [{
+          name: 'Prosciuto & Funghi',
+          img: '../assets/imgs/dishes/pizza1.png',
+          description: '30 cm, Mozzarela, Ham, Mushrooms, Tomato Sauce',
+          price: 26.00,
+        },
+        {
+          name: 'Pasta Alfredo',
+          img: '../assets/imgs/dishes/pizza1.png',
+          description: 'Fettuccine Pasta With Fresh Mushroom in Fresh Creamy',
+          price: 37.00,
+        }]
+    },
+    {
+      category: 'snacks',
+      itemsShown: false,
+      items: [{
+          name: 'Prosciuto & Funghi',
+          img: '../assets/imgs/dishes/pizza1.png',
+          description: '30 cm, Mozzarela, Ham, Mushrooms, Tomato Sauce',
+          price: 26.00,
+        },
+        {
+          name: 'Pasta Alfredo',
+          img: '../assets/imgs/dishes/pizza1.png',
+          description: 'Fettuccine Pasta With Fresh Mushroom in Fresh Creamy',
+          price: 37.00,
+        }]
+    },
+    {
+      category: 'pizza',
+      itemsShown: false,
+      items: [{
+          name: 'Prosciuto & Funghi',
+          img: '../assets/imgs/dishes/pizza1.png',
+          description: '30 cm, Mozzarela, Ham, Mushrooms, Tomato Sauce',
+          price: 26.00,
+        },
+        {
+          name: 'Pasta Alfredo',
+          img: '../assets/imgs/dishes/pizza1.png',
+          description: 'Fettuccine Pasta With Fresh Mushroom in Fresh Creamy',
+          price: 37.00,
+        }]
+    },
+    {
+      category: 'coffee / chocolate',
+      itemsShown: false,
+      items: [{
+          name: 'Prosciuto & Funghi',
+          img: '../assets/imgs/dishes/pizza1.png',
+          description: '30 cm, Mozzarela, Ham, Mushrooms, Tomato Sauce',
+          price: 26.00,
+        },
+        {
+          name: 'Pasta Alfredo',
+          img: '../assets/imgs/dishes/pizza1.png',
+          description: 'Fettuccine Pasta With Fresh Mushroom in Fresh Creamy',
+          price: 37.00,
+        }]
+    }]
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -15,5 +80,13 @@ export class BeachPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad BeachPage');
   }
+
+  toggleItems(item) {
+    item.itemsShown = !item.itemsShown
+    this.menu.forEach((element) => {
+      if (element != item) element.itemsShown = false
+    })
+  }
+
 
 }
